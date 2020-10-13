db8c721c4368c992338bfc0243527b3980cb835b
eliasneri <eliasneri@hotmail.com>
2020-10-13 17:14:00 -0300

    Implementação Classe Accountgit add .!

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -4,4 +4,42 @@ public class Account {
 	private Integer number;
 	private String holder;
 	private Double balance;
+	
+	public Account() {
+		
+	}
+	
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+	
+	public String getHolder() {
+		return holder;
+	}
+	
+	public Double balance() {
+		return balance;
+	}
+	
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void whitdraw(double amount) {
+		balance -= amount;
+	}
 }
